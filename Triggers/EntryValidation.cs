104ede9c1d822ee01a2888d8883f5d1938feb1ea
Richard Hill <richardhill101@gmail.com>
2017-11-20 13:19:14 +0000

    First Commit

---
+++ Triggers/EntryValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Triggers
+{
+    public class EntryValidation : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry sender)
+        {
+            int parsed;
+            bool valid = int.TryParse(sender.Text, out parsed);
+
+            if(!valid)
+            {
+                sender.TextColor = Color.Red;
+            }
+            else
+            {
+                sender.TextColor = Color.Blue;
+            }
+        }
+    }
+}
